57f7b29a244e760911fabe090cb8c803ce46bbdf
Natalia Tigh <natih6218@gmail.com>
2019-06-09 11:20:38 -0700

    add new values and sstyle

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
 $('form#container').submit(function(event){
+    event.preventDefault();
   var question1 = $("input:radio[name=purpose]:checked").val();
   var question2 = $("input:radio[name=activities]:checked").val();
   var question3 = $("input:radio[name=food]:checked").val();
@@ -8,12 +9,14 @@ $('form#container').submit(function(event){
   var question5 = $("input:radio[name=season]:checked").val();
 
 
-if (question1==="relax" && question2 === "walk" && question3 === "vegetarian") {
-  $('#place2').show()
+if (question1=== "party" && question2 === "dance" && question3 === "fastfood" && question4 === "salsa" && question5=== "summer") {
+  $('#place1').show();
+} else if (question1 === "learn" && question2 === "walk" && question3 === "vegetarian" && question4 === "classic" && question5 === "spring") {
+  $('#place2').show();
 } else {
   $('#place3').show();
 }
-  event.preventDefault();
+
 });
 
 });
